b591bc13d8d2c73cc32d3b620cbdbf9ea37242df
github username <63208817+Thea-J@users.noreply.github.com>
2020-11-01 15:47:40 +0100

    Describe HTTP verbs & controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #The 7 RESTful routes
+  #REST is an architectural design pattern
+
+  #HTTP varb -> URL path -> Controller -> Action (Performs CRUD, database query & renders views)
 end
