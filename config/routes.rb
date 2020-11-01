01c16e0f9a5b7cda43a3bf9db1f58f5250399168
github username <63208817+Thea-J@users.noreply.github.com>
2020-11-01 16:30:15 +0100

    Explicitly draw 7 dynamically RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #The 7 RESTful routes
-  #REST is an architectural design pattern
+  #The 7 RESTful routes - REST is an architectural design pattern
+  #HTTP verb -> URL path -> Controller -> Action (Performs CRUD, database query & renders views)
 
-  #HTTP varb -> URL path -> Controller -> Action (Performs CRUD, database query & renders views)
+  # 1) Draw the routes: HTTP verb "URL path", to: "controller#action"
+
+  get "/posts", to: "posts#index"
+
+  get "/posts/:id", to: "posts#show"
+
+  get "/posts/new", to: "posts#new"
+
+  post "/posts", to: "posts#create"
+
+  get "/posts/:id/edit", to: "posts#edit"
+
+  patch "/posts/:id", to: "posts#update"
+
+  delete "/posts/:id", to: "posts#destroy"
+
 end
